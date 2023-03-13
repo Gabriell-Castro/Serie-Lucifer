5428d92f41455f1dad1949ceec1857e6291de30b
Gabriel França <113274275+Gabriell-Castro@users.noreply.github.com>
2023-03-13 00:01:14 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# The-Last-of-Us
-Esse projeto foi desenvolvido com as tecnologias:  - HTML e CSS - JavaScript
+<h1 align="center"> The Last Of Us </h1>
+
+<br>
+
+<p align="center">
+  <img alt="projeto Carrossel Série" src=".github/thelastofus.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+
+- [Acesse o projeto finalizado, online](https://omarcosabreu.github.io/devEmDobro-TheLastOfUs)
+
+---
---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const botoesCarrossel = document.querySelectorAll(".botao");
+const imagemCarrossel = document.querySelectorAll(".imagem");
+
+botoesCarrossel.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    const botaoSelecionado = document.querySelector(".selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+
+    botao.classList.add("selecionado");
+
+    const imagemAtiva = document.querySelector(".ativa");
+    imagemAtiva.classList.remove("ativa");
+
+    imagemCarrossel[indice].classList.add("ativa");
+  });
+});
